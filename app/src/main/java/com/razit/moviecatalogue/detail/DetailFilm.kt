767137e89a634115f257bbf4c7df377c8956bc88
Razit <razituli@gmail.com>
2021-05-11 16:54:16 +0700

    Menghilangkan string hardcore pada support actionbar title

--- app/src/main/java/com/razit/moviecatalogue/detail/DetailFilm.kt
+++ app/src/main/java/com/razit/moviecatalogue/detail/DetailFilm.kt
@@ -13,41 +13,42 @@ import com.razit.moviecatalogue.viewmodel.FilmViewModel
 
 class DetailFilm : AppCompatActivity() {
 
-    companion object{
+    companion object {
         const val ID = "idFilm"
         const val CATEGORY = "category"
     }
-    private lateinit var film : FilmEntity
+
+    private lateinit var film: FilmEntity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val activityDetailFilm = ActivityDetailFilmBinding.inflate(layoutInflater)
         setContentView(activityDetailFilm.root)
-        supportActionBar?.title = "Detail Film"
+        supportActionBar?.setTitle(R.string.detail_film)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
-        val viewModel =  ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())[FilmViewModel::class.java]
+        val viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())[FilmViewModel::class.java]
 
 
         val extra = intent.extras
 
-        if(extra != null){
+        if (extra != null) {
             val id = extra.getInt(ID)
             val category = extra.getString(CATEGORY)
-            film = if(category == FilmFragment.MOVIES){
+            film = if (category == FilmFragment.MOVIES) {
                 viewModel.getDetailMovies(id)
-            }else{
+            } else {
                 viewModel.getDetailTvShow(id)
             }
 
-            with(activityDetailFilm){
+            with(activityDetailFilm) {
                 Glide.with(this@DetailFilm)
-                    .load(film.imageUrl)
-                    .apply(
-                            RequestOptions.placeholderOf(R.drawable.ic_loading)
-                                    .error(R.drawable.ic_error))
-                    .into(imgPoster)
+                        .load(film.imageUrl)
+                        .apply(
+                                RequestOptions.placeholderOf(R.drawable.ic_loading)
+                                        .error(R.drawable.ic_error))
+                        .into(imgPoster)
                 tvGenre.text = film.genre
-                tvDesc.text=film.description
+                tvDesc.text = film.description
                 tvTitle.text = film.title
                 tvDate.text = film.release
                 tvDirector.text = film.director
